7bc0fa9c9c99ef89464820e85c09f772f94d0009
Ian Zhu <yzhu132@myseneca.ca>
2018-06-12 01:41:06 -0400

    Delete 90 min shutdown - .bat

--- 90 min shutdown - .bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-:myLabel
-SHUTDOWN /S /F /T 5400
-SET /P continue="Your computer is about to shutdown in 90 min do you want to abort (y/n): "
-IF %continue% EQU y (
-SHUTDOWN /A
-TIMEOUT /T 1800 /NOBREAK
-GOTO myLabel
-)
